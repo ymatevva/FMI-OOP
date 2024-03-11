22fd7b1697c4636c71dd5a1a29cca2640713ca63
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2024-03-11 15:53:05 +0200

    Add files via upload

---
+++ Practicum Information Systems 2ndGroup/Week 03/Task4..cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<fstream>
+
+namespace HelperFunctions {
+
+	bool isLower(char ch) {
+		return ch >= 'a' && ch <= 'z';
+	}
+
+	bool isUpper(char ch) {
+		return ch >= 'A' && ch <= 'Z';
+	}
+
+	char toLower(char ch) {
+		return ch - 'A' + 'a';
+	}
+
+	char toUpper(char ch) {
+		return ch - 'a' + 'A';
+	}
+}
+
+void transformSentence(char* str, bool toCaps) {
+	
+	if (!str)
+		return;
+
+	while (*str) {
+
+		if (toCaps && HelperFunctions::isLower(*str))
+			*str = HelperFunctions::toUpper(*str);
+
+		 if(!toCaps && HelperFunctions::isUpper(*str))
+			*str = HelperFunctions::toLower(*str);
+
+		str++;
+	}
+}
+
+
+void copyInformation(const char* sourceFile, const char* outputFile) {
+
+	if (!sourceFile || !outputFile)
+		return;
+
+	std::ifstream ifs(sourceFile);
+	if (!ifs.is_open())
+		return;
+
+	std::ofstream ofs(outputFile);
+	if (!ofs.is_open())
+		return;
+
+	while (!ifs.eof()) {
+		char buff[1024];
+		ifs.getline(buff, 1024);
+		ofs << buff << std::endl;
+	}
+}
+
+
+void convertLettersCaseInFile(const char* fileName, bool toCaps) {
+
+	if (!fileName)
+		return;
+
+	std::ifstream ifs(fileName);
+	if (!ifs.is_open())
+		return;
+
+
+	std::ofstream ofs("temp.txt");
+
+	if (!ofs.is_open()){
+		std::cout << "The temp file was not opened" << std::endl;
+		return;
+	}
+
+
+	char buffer[1024];
+
+	while (ifs.getline(buffer, 1024)) {
+		transformSentence(buffer, toCaps);
+		ofs << buffer << std::endl;
+	}
+
+	copyInformation("temp.txt", fileName);
+}
+
+int main() {
+
+	convertLettersCaseInFile("Test.txt", true);
+
+	return 0;
+}
