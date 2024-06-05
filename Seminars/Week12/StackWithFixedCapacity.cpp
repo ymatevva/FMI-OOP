c29d9082995a38ad1ce65185e6022b4220e1c466
Йоана Матева <107748308+YoanaSM@users.noreply.github.com>
2024-06-05 21:52:29 +0300

    Adding task1 from seminar12

---
+++ Seminars/Week12/StackWithFixedCapacity.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<exception>
+
+template<typename T, const unsigned S>
+class MyStack {
+
+    T data[S];
+    size_t size = 0;
+
+public:
+    void push(const T& obj);
+    void push(T&& obj);
+
+    void pop();
+    const T& peek() const;
+
+    bool isEmpty();
+};
+
+
+template<typename T, const unsigned S>
+void MyStack<T, S>::push(const T& obj) {
+    if (size >= S)
+        return;
+
+    data[size++] = obj;
+}
+
+
+template<typename T, const unsigned S>
+void MyStack<T, S>::push(T&& obj) {
+    if (size >= S)
+        return;
+
+    data[size++] = std::move(obj);
+}
+
+template<typename T, const unsigned S>
+void MyStack<T, S>::pop() {
+    if (isEmpty()) {
+        throw std::out_of_range("The data array is already empty.");
+    }
+
+    size--;
+}
+
+template<typename T, const unsigned S>
+bool MyStack<T, S> ::isEmpty() {
+    return size == 0;
+}
+
+template<typename T, const unsigned S>
+const T& MyStack<T, S>::peek() const {
+    if (isEmpty()) {
+        throw std::out_of_range("There are no elements left.");
+    }
+
+    return data[size - 1];
+}
